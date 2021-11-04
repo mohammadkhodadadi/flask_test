ec25f6e3dc0733354ac5fa075bcadbaca5f7277b
mohammadkhodadadi <mkhodadadi92@yahoo.com>
2021-11-04 13:46:59 +0330

    Create main.py
    add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import Flask, redirect, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/x/info")
+def info():
+    first_name = request.args.get("first_name")
+    return first_name.upper()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8080', debug=True)
